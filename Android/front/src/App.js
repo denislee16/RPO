f86500291c1373445fb790e86fff1fd926c8d76a
Denis Lee <berserger04@mail.ru>
2025-04-18 14:21:06 +0300

    Лабораторная работа 8,9

---
+++ Android/front/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+ import React from "react";
+ import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import {createBrowserHistory} from "history";
+
+ import NavigationBar from "./components/NavigationBar";
+ import Home from "./components/Home";
+ import Login from "./components/Login"
+ 
+ function App() {
+     return (
+         <div className="App">
+             <BrowserRouter>
+                 <NavigationBar />
+                 <div className="container-fluid">
+                     <Routes>
+                         <Route path="home" element={<Home />}/>
+                     </Routes>
+                 </div>
+                  <Login />
+             </BrowserRouter>
+         </div>
+     );
+ }
+ 
+ export default App;
